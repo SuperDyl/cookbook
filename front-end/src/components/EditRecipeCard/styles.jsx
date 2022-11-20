6382b787b191b842157d8132c89a445262dd006c
Dylan <SuperDyl19@gmail.com>
2022-11-20 14:01:36 -0700

    edit styles based on display styles

--- front-end/src/components/EditRecipeCard/styles.jsx
+++ front-end/src/components/EditRecipeCard/styles.jsx
@@ -1,48 +1,28 @@
 import styled from "styled-components";
 
-export const CardBody = styled.div`
-  text-align: left;
-  padding: 16px;
-  margin: 24px;
-  background-color: #ffd3d0;
-  border: red thick dotted;
-`;
+import {
+  CardBody as DisplayCardBody,
+  TitleBox as DisplayTitleBox,
+  DishTitle as DisplayDishTitle,
+  IngredientList as DisplayIngredientList,
+  IngredientItem as DisplayIngredientItem,
+  StepsList as DisplayStepsList,
+  NotesText as DisplayNotesText,
+  EditButton as DisplayEditButton
+} from "../DisplayRecipeCard/styles";
 
-export const TitleBox = styled.span`
-  display: flex;
-  justify-content: space-between;
-`;
+export const CardBody = DisplayCardBody;
 
-export const DishTitle = styled.h2`
-  margin: 0;
-  display: inline;
-`;
+export const TitleBox = DisplayTitleBox;
 
-export const IngredientList = styled.ul`
-  padding-left: 0;
-  margin-left: 20px;
-  list-style-type: circle;
-`;
+export const DishTitle = DisplayDishTitle;
 
-export const IngredientItem = styled.li`font-style: italic;`;
+export const IngredientList = DisplayIngredientList;
 
-export const StepsList = styled.ol`
-  padding-left: 0;
-  margin-left: 24px;
-`;
+export const IngredientItem = DisplayIngredientItem;
 
-export const NotesText = styled.p`font-weight: bold;`;
+export const StepsList = DisplayStepsList;
 
-export const EditButton = styled.button`
-  padding: 0;
-  border: none;
-  background: none;
+export const NotesText = DisplayNotesText;
 
-  :hover {
-    border: black thin solid;
-  }
-
-  :active {
-    background-color: white;
-  }
-`;
+export const EditButton = DisplayEditButton;
