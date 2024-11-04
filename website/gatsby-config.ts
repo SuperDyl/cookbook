c771b3bfac34e0f62bf585ac73eac2653851ec46
Dylan <SuperDyl19@gmail.com>
2024-11-04 11:44:07 -0700

    Remove gatsby defaults

--- website/gatsby-config.ts
+++ website/gatsby-config.ts
@@ -1,15 +1,12 @@
 import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
-  siteMetadata: {
-    title: `cookbook-website`,
-    siteUrl: `https://cookbook.superdyl.net`
-  },
-  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
-  // If you use VSCode you can also use the GraphQL plugin
-  // Learn more at: https://gatsby.dev/graphql-typegen
-  graphqlTypegen: true,
-  plugins: ["gatsby-plugin-styled-components"]
+	siteMetadata: {
+		title: `superdyl-cookbook`,
+		siteUrl: `https://cookbook.superdyl.net`,
+		description: `Upload, share, edit, and print your recipes`,
+	},
+	plugins: ["gatsby-plugin-styled-components"],
 };
 
 export default config;
---
+++ website/src/components/Layout/index.tsx
@@ -0,0 +1,25 @@
+import React, { ReactNode } from "react";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    min-width: 100vw;
+    min-height: 100vh;
+    background-color: #263728;
+  }
+`;
+
+type LayoutProps = {
+	children?: ReactNode;
+};
+
+const Layout: React.FunctionComponent<LayoutProps> = ({ children }) => {
+	return (
+		<>
+			<GlobalStyle />
+			{children}
+		</>
+	);
+};
+
+export default Layout;
