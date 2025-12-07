4e00562d12c9e71da27fcb26822182e474ceaccb
Dylan <SuperDyl19@gmail.com>
2025-12-06 19:58:17 -0700

    Fix Connection transactions and bulk.

--- server/src/sql/connection.ts
+++ server/src/sql/connection.ts
@@ -9,14 +9,14 @@ export default class Connection {
 
     public transaction<T>(transactionCallback: () => T) {
         const savepointName = `save_${crypto.randomUUID().replaceAll(/-/g,'_')};`;
-        this.run`savepoint ${savepointName};`;
+        this.exec(`savepoint ${savepointName};`);
         try {
             const result = transactionCallback();
-            this.run`release savepoint ${savepointName};`;
+            this.exec(`release savepoint ${savepointName};`);
             return result;
         } catch (e: unknown) {
             console.error('Received error during transaction, rolling back', e);
-            this.run`rollback to savepoint ${savepointName};`;
+            this.exec(`rollback to savepoint ${savepointName};`);
             throw e;
         }
     }
@@ -36,13 +36,18 @@ export default class Connection {
         return statement.run(...parameters) as { changes: number, lastInsertRowid: number };
     }
 
+    public exec(sql: string): void {
+        this._db.exec(sql);
+    }
+
     public bulk(strings: TemplateStringsArray, ...parameterLists: any[][])
         : { changes: number, lastInsertRowid: number }[] {
         return this.transaction(() => {
             const statement = this._db.prepare(strings.join('?'));
             const results = [];
 
-            for (const parameters of parameterLists) {
+            for (let i = 0; i < parameterLists[0].length; i++) {
+                const parameters = parameterLists.map(list => list[i]);
                 results.push(statement.run(...parameters) as { changes: number, lastInsertRowid: number });
             }
 
